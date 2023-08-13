afdf88916c42f0934c259c4fff9a0d05796734de
Dennis Bozzi <dennisbozzii@gmail.com>
2023-08-12 23:55:10 -0300

    Adicionar arquivos de projeto.

---
+++ Construtores/Pessoa.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreGenealogica.Construtores
+{
+    internal class Pessoa
+    {
+
+        //Atributos de uma nova Pessoa
+        public String nome;
+        public Pessoa conjuge;
+        public List<Pessoa> filhos;
+
+        //Construtor de uma Pessoa
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+            filhos = new List<Pessoa>();
+        }
+
+        //Construtor que adiciona um filho a pessoa referenciada
+        public Pessoa(string nome, Pessoa pessoa)
+        {
+            this.nome = nome;
+            filhos = new List<Pessoa>();
+            pessoa.AdicionarFilho(this);
+        }
+
+        //Adicionar Filho
+        public void AdicionarFilho(Pessoa novaPessoa)
+        {
+            filhos.Add(novaPessoa);
+        }
+
+        //Adiciona um Conjuge a uma pessoa e vice versa
+        public void Conjuge(Pessoa pessoa)
+        {
+            conjuge = pessoa;
+            pessoa.conjuge = this;
+        }
+
+        //Imprime e Árvore Genealógica
+        public void ImprimeArvore(int nivelIndentacao = 0)
+        {
+            string espacos = new string(' ', nivelIndentacao * 3);
+
+            if (conjuge != null && filhos.Count > 0)
+            {
+                Console.WriteLine($"{espacos}{nome} é casado(a) com {conjuge.nome}, filhos:");
+            } else if (conjuge != null)
+            {
+                Console.WriteLine($"{espacos}{nome} é casado(a) com {conjuge.nome}");
+            }
+            else
+            {
+                Console.WriteLine($"{espacos}{nome} é solteiro(a).");
+            }
+
+            if (filhos.Count > 0)
+            {
+                foreach (var filho in filhos)
+                {
+                    filho.ImprimeArvore(nivelIndentacao + 1);
+                }
+            }
+        }
+    }
+
+
+}
---
+++ TestaArvore.cs
@@ -0,0 +1,61 @@
+﻿using ArvoreGenealogica.Construtores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreGenealogica
+{
+    internal class TestaArvore
+    {
+        static void Main()
+        {
+            Pessoa Lily = new Pessoa("Lily");
+            Lily.Conjuge(new Pessoa("Wilhelm"));
+
+            // Patriarcas
+            Pessoa Opa = new Pessoa("Opa", Lily);
+            Opa.Conjuge(new Pessoa("Oma"));
+
+            // Filhos de Oma e Opa
+            Pessoa Reinhold = new Pessoa("Reinhold", Opa);
+            Pessoa Wilma = new Pessoa("Wilma", Opa);
+            Pessoa Sigrid = new Pessoa("Sigrid", Opa);
+
+            // Filhos e esposa de Reinhold
+            Reinhold.Conjuge(new Pessoa("Sonia"));
+            Pessoa Christian = new Pessoa("Christian", Reinhold);
+            Pessoa Gabrielle = new Pessoa("Gabrielle", Reinhold);
+            Pessoa Sabine = new Pessoa("Sabine", Reinhold);
+
+            // Filhos e marido de Wilma
+            Wilma.Conjuge(new Pessoa("Rodolfo"));
+            Pessoa Ricardo = new Pessoa("Ricardo", Wilma);
+            Pessoa Rodrigo = new Pessoa("Rodrigo", Wilma);
+
+            // Ricardo e esposa
+            Ricardo.Conjuge(new Pessoa("Debora"));
+
+            // Filhos e esposa de Christian
+            Christian.Conjuge(new Pessoa("Mônica"));
+            Pessoa Oscar = new Pessoa("Oscar", Christian);
+            Pessoa Lorena = new Pessoa("Lorena", Christian);
+
+            // Filhos e marido Sigrid
+            Sigrid.Conjuge(new Pessoa("Peter"));
+            Pessoa Martin = new Pessoa("Martin", Sigrid);
+            Pessoa Thomas = new Pessoa("Thomas", Sigrid);
+            Pessoa Claudia = new Pessoa("Claudia", Sigrid);
+
+            Martin.Conjuge(new Pessoa("Carla"));
+            Martin.AdicionarFilho(new Pessoa("Nicolas"));
+
+            Lily.ImprimeArvore();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
